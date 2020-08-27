4f755d273d5f39234449f1cb7768ef5da2d4e0b2
Diogo Figueiredo <diogo.alvesfigueiredo@gmail.com>
2020-08-27 17:57:22 +0100

    alert dialogues

--- app/src/main/java/com/example/kotlinalertdialogues/MainActivity.kt
+++ app/src/main/java/com/example/kotlinalertdialogues/MainActivity.kt
@@ -1,11 +1,29 @@
 package com.example.kotlinalertdialogues
 
+import android.content.DialogInterface
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun save(view: View) {
+        val alert = AlertDialog.Builder(this)
+
+        alert.setTitle("Save")
+        alert.setMessage("Are you sure you want to save?")
+        alert.setPositiveButton("Yes") {
+                dialogInterface: DialogInterface, i:Int -> Toast.makeText(applicationContext,"Saved",Toast.LENGTH_SHORT).show()
+        }
+        alert.setNegativeButton("No") {
+                dialogInterface: DialogInterface, i:Int -> Toast.makeText(applicationContext,"Discarded",Toast.LENGTH_SHORT).show()
+        }
+        alert.show()
     }
 }
